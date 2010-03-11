09e9844dac29be6e1464edeb2d35c63194dff4c1
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-03-11 14:50:31 +0000

    renamed packages

---
+++ src/main/java/de/cosmocode/palava/ipc/cache/CachePolicy.java
@@ -0,0 +1,55 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007-2010  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.cosmocode.palava.ipc.cache;
+
+/**
+ * A CachePolicy decides how to cache a {@link Command}.
+ * 
+ * @author Willi Schoenborn
+ * @author Oliver Lorenz
+ */
+public enum CachePolicy {
+
+    /**
+     * <p> A CachePolicy where the fully qualified name of the command-class,
+     * the {@linkplain IpcCall#getArguments() Arguments} of a Call
+     * and the {@linkplain HttpSession#getLocale() Locale} of the Session of a Call
+     * are taken into consideration for caching.
+     * </p>
+     * <h4>This policy behaves differently if nulls occur: </h4>
+     * <ul>
+     *   <li> If the locale is null, then no caching occurs. </li> 
+     *   <li> If the arguments are null, then they are ignored 
+     *        and only the locale and the fully qualified name are taken for caching </li>
+     * </ul>
+     */
+    SMART,
+    
+    /**
+     * <p> A CachePolicy where only the fully qualified name of the command-class
+     * is used for caching.
+     * </p>
+     * <p> This means that the Command annotated with this CachePolicy
+     * always returns the same Content, independent of the call.
+     * </p>
+     */
+    STATIC;
+    
+}
---
+++ src/test/java/de/cosmocode/palava/ipc/cache/SimpleCall.java
@@ -0,0 +1,146 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007-2010  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/**
+ * 
+ */
+package de.cosmocode.palava.ipc.cache;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Maps;
+import com.google.common.collect.UnmodifiableIterator;
+
+import de.cosmocode.collections.utility.Utility;
+import de.cosmocode.collections.utility.UtilityMap;
+import de.cosmocode.collections.utility.UtilitySet;
+import de.cosmocode.palava.ipc.AbstractIpcArguments;
+import de.cosmocode.palava.ipc.IpcArguments;
+import de.cosmocode.palava.ipc.IpcCall;
+import de.cosmocode.palava.ipc.IpcConnection;
+
+/**
+ * A simple {@link Call} that saves the given command, request
+ * and the Strings as arguments,
+ * taking every other argument as the value to the previous argument.
+ * 
+ * @author Oliver Lorenz
+ */
+final class SimpleCall implements IpcCall {
+    
+    private final IpcArguments arguments;
+    private final IpcConnection request;
+    private final Map<Object, Object> context = Maps.newHashMap();
+    
+    /**
+     * Saves the given connection as HttpRequest.
+     * The Strings are taken as arguments,
+     * treating every other argument as the value to the previous argument.
+     * @param request
+     * @param arguments
+     */
+    public SimpleCall(final IpcConnection connection, final String... arguments) {
+        this.request = connection;
+        if (arguments.length > 0) {
+            this.arguments = new SimpleArguments(arguments);
+        } else {
+            this.arguments = null;
+        }
+    }
+
+    @Override
+    public IpcArguments getArguments() {
+        return arguments;
+    }
+    
+    @Override
+    public IpcConnection getConnection() {
+        return request;
+    }
+    
+    @Override
+    public <K> boolean contains(K key) {
+        return context.containsKey(key);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <K, V> V get(K key) {
+        return (V) context.get(key);
+    }
+
+    @Override
+    public <K, V> void putAll(Map<? extends K, ? extends V> map) {
+        context.putAll(map);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <K, V> V remove(K key) {
+        return (V) context.remove(key);
+    }
+
+    @Override
+    public <K, V> void set(K key, V value) {
+        context.put(key, value);
+    }
+
+    @Override
+    public UnmodifiableIterator<Entry<Object, Object>> iterator() {
+        return Iterators.unmodifiableIterator(context.entrySet().iterator());
+    }
+    
+    /**
+     * The backing Arguments class for the SimpleCall.
+     * 
+     * @author Oliver Lorenz
+     */
+    private static final class SimpleArguments extends AbstractIpcArguments {
+        
+        private final UtilityMap<String, Object> forwarded;
+        
+        public SimpleArguments(final String... arguments) {
+            final Map<String, Object> map = Maps.newHashMap();
+            String prevArg = null;
+            for (final String arg : arguments) {
+                if (prevArg != null) {
+                    map.put(prevArg, arg);
+                    prevArg = null;
+                } else {
+                    prevArg = arg;
+                }
+            }
+            this.forwarded = Utility.asUtilityMap(map);
+        }
+
+        @Override
+        public UtilitySet<Entry<String, Object>> entrySet() {
+            return forwarded.entrySet();
+        }
+
+        @Override
+        public Object put(String key, Object value) {
+            return forwarded.put(key, value);
+        }
+        
+    }
+    
+}
